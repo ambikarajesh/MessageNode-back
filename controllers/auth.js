bc9d2965c4341bb7e62186d0787a512ee716254f
ambikarajesh <ambikula@gmail.com>
2019-04-06 22:38:33 -0700

    get and update status done

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,3 +56,45 @@ exports.postLogin = (req, res, next) => {
     })  
    
 }
+
+exports.getStatus = (req, res, next) =>{
+    User.findById(req.userId).then(user =>{
+        if(!user){
+            const error = new Error('Not user found.');
+            error.statusCode = 401;        
+            throw error;
+        }
+        res.status(200).json({
+            message:'Fetch status successfully',
+            status: user.status
+        })
+    }).catch(err =>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    }) 
+}
+
+exports.postStatus = (req, res, next) =>{
+    const status = req.body.status;
+    console.log(status)
+    User.findById(req.userId).then(user =>{
+        if(!user){
+            const error = new Error('Not user found.');
+            error.statusCode = 401;        
+            throw error;
+        }
+        user.status = status;
+        return user.save();
+    }).then(result => {
+        res.status(200).json({
+            message:'Updated status successfully',
+        })
+    }).catch(err =>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    }) 
+}
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {check} = require('express-validator/check');
 const User = require('../models/user');
 const authController = require('../controllers/auth');
+const isAuth = require('../middleware/isAuth');
 const router = express.Router();
 
 router.put('/signup',[check('email').isEmail().withMessage('Please Enter Valid Email !!!').normalizeEmail().custom((value, {req})=>{
@@ -15,4 +16,6 @@ router.put('/signup',[check('email').isEmail().withMessage('Please Enter Valid E
                     check('name').trim().not().isEmpty(),
                     check("password").isLength({min:8}).withMessage("Password Should be Combination of One Uppercase , One Lower case, One Special Char, One Digit and atleast 8 Charaters !!!").trim()], authController.putSignup);
 router.post('/login', authController.postLogin)
+router.get('/status', isAuth, authController.getStatus);
+router.post('/status', isAuth, authController.postStatus);
 module.exports = router;
