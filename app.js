b5c0c89a5453c69ee1b19a7694b0600770cd85e9
ambikarajesh <ambikula@gmail.com>
2019-04-11 17:07:55 -0700

    header changed

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(multer({storage:fileStorage, fileFilter:fileFilter}).single('image'))
 app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     next();
 })
 app.use(helmet());
